ee01ba3da20c85875ad893c6d39fbb94f5b3f84e
Dario Clavijo <daedalus2027@gmail.com>
2016-12-01 13:18:59 -0200

    str tests

---
+++ teststr.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import sys
+import fileinput
+
+P = 115792089237316195423570985008687907853269984665640564039457584007908834671663L
+N = 115792089237316195423570985008687907852837564279074904382605163141518161494337L
+
+
+
+#skip = int("00000000000000000000000000000000000000000000000000000027eb78574a",16)
+skip = 0
+
+def hexify(i):
+	return hex(i).replace('0x','').replace('L','').zfill(64)
+
+data = []
+for line in fileinput.input():
+	line = line.replace('0x','').replace('L','').replace('\n','')	
+	try:
+		data.append(line)	
+	except:
+		pass
+
+#import random
+#random.shuffle(data)
+
+start = 0
+end = len(data) -1
+mid = int(end / 2)
+
+def prepare(data):
+	data2 = []
+	for k in range(mid,end):
+		data2.append(data[k])
+	for k in range(mid,start,-1):
+		data2.append(data[k])
+
+	return data2
+
+def invert(data):
+	data2 = []
+	for k in range(start,end):
+		data2.append(data[k])
+	return data2
+
+for i in range(start,end):
+	for j in range(start,end):
+		#if i != j:
+		s = data[i] + data[j]
+		print s
+		print s.title()
+		s = data[i] + " " + data[j]
+		print s
+		print s.title()
+		s = data[i].lower() + data[j].lower()
+		print s
+		print s.title()
+		s = data[i].lower() + " " + data[j].lower()
+		print s
+		print s.title()
+		s = data[i].upper() + data[j].upper()
+		s = data[i].upper() + " " + data[j].upper()
+		s = data[i].lower() + data[j].upper()
+		print s
+		print s.title()
+		s = data[i].lower() + " " + data[j].upper()
+		print s.title()
+		s = data[i].upper() + data[j].lower()
+		print s
+		print s.title()
+		s = data[i].upper() + " " + data[j].lower()
+		print s
+		print s.title()
+
+
+			
+
+
---
+++ teststr3.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import sys
+import fileinput
+
+P = 115792089237316195423570985008687907853269984665640564039457584007908834671663L
+N = 115792089237316195423570985008687907852837564279074904382605163141518161494337L
+
+
+
+#skip = int("00000000000000000000000000000000000000000000000000000027eb78574a",16)
+skip = 0
+
+def hexify(i):
+	return hex(i).replace('0x','').replace('L','').zfill(64)
+
+data = []
+for line in fileinput.input():
+	line = line.replace('\n','').replace('\r','')	
+	data.append(line)
+	
+
+def join2(a,b):
+	
+	print a.title() + " " + b
+	print a.title() + b
+	print a + " " + b.title()
+	print a + b.title()
+	print a.title() + " " + b.title()
+	print a.title() + b.title()
+	print a.lower() + " " + b.lower()
+	print a.lower() + b.lower()
+	print a.upper() + " " + b.upper()
+	print a.upper() + b.upper()
+	print a.lower() + " " + b.upper()
+	print a.lower() + b.upper()
+	print a.upper() + " " + b.lower()
+	print a.upper() + b.lower()
+	print a + " " + b
+	print a + b
+
+for i in range(0,len(data)-1):
+	for j in range(0,len(data)-1):
+		join2(data[i], data[j])
