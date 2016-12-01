167e7a0f4432d9e0f22852798a32e3ae582f0d36
Dario Clavijo <daedalus2027@gmail.com>
2016-12-01 13:29:58 -0200

    test bit patterns

---
+++ testbitpatterns.py
@@ -0,0 +1,21 @@
+def hexify(i):
+	return hex(i).replace('0x','').replace('L','').zfill(64)
+
+def test(i,a):
+	b = ""
+	while len(b) <= 256:
+		if a:
+			b = bin(i).replace('0b','') + b
+		else:	
+			b += bin(i).replace('0b','')
+		i += 1
+	return hexify(int(b,2))
+
+i = 0
+while True:
+	i += 1
+	print test(i,False)
+	print test(i,True)
+
+
+
