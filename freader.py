531a9b11d28a68591bc54fb1167ab64954c41d85
Dario Clavijo <daedalus2027@gmail.com>
2017-04-20 21:59:28 -0300

    some improvements

--- freader.py
+++ freader.py
@@ -7,24 +7,24 @@ import os
 import math
 import io
 
-def shannon_entropy(data, iterator):
+def shannon_entropy(data, iterator=None):
     """
     Borrowed from http://blog.dkbza.org/2007/05/scanning-data-for-entropy-anomalies.html
     """
     if not data:
         return 0
     entropy = 0
+
+    if iterator==None:
+        SET=""
+        for i in range(0,255):
+            SET+=chr(i)
+
     for x in (ord(c) for c in iterator):
         p_x = float(data.count(chr(x)))/len(data)
         if p_x > 0:
             entropy += - p_x*math.log(p_x, 2)
     return entropy
-
-
-
-SET=""
-for i in range(0,255):
-	SET+=chr(i)
 
 thresshold = 4.5
 
@@ -32,7 +32,7 @@ filename=sys.argv[1]
 #fp = open(filename)
 fp = io.BufferedReader(io.FileIO(filename,'rb'))
 
-i = int(sys.argv[2])
+start = int(sys.argv[2])
 l = int(sys.argv[3])
 flen=os.stat(filename).st_size
 if flen == 0:
@@ -40,10 +40,10 @@ if flen == 0:
 
 while True:
 	if i <= flen:
-		fp.seek(i,0)
+		fp.seek(start,0)
 		data = fp.read(l)
 
-		e = shannon_entropy(data,SET)
+		e = shannon_entropy(data)
 
 		#print data.encode('hex'),e
  
