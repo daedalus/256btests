4b3e8a67db23ff9f50caf5f69b4725432cd0486e
Dario Clavijo <daedalus2027@gmail.com>
2018-09-10 16:28:28 -0300

    simple bit frequency analysis test

---
+++ simple_stat_analysis.py
@@ -0,0 +1,18 @@
+import fileinput
+
+bits = 256
+freqs = []
+
+for i in range(0,bits):
+	freqs.append(0)
+
+c=0
+for line in fileinput.input():
+	b = format(int(line,16),'08b').zfill(256)
+	for i in range(0,bits):
+		if b[i] == "1":
+			freqs[i] +=1
+	c+=1
+
+print freqs
+	
